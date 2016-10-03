69c6bca29d7194e36f5f01aff140dffffb559f68
Anne Fraysse <fraysse.anne@gmail.com>
2016-10-03 00:27:53 -0700

    adding file reading.

--- files.py
+++ files.py
@@ -13,3 +13,18 @@ myfile.close()
 # open(fileName, mode) - takes 2 arguments
 # mode - there are different modes dependent upon functionality 
     # "w" - open the file for writing 
+# with mode 'w' if there is a file called test.txt, it will be replaced
+    # - if there is no test.txt, one will be created
+
+""" READING FILES """
+
+# open in mode 'r' - reading mode 
+mynewhandle = open("test.txt", "r")
+while True:                             # keep reading forever
+    theline = mynewhandle.readline()    # try to read next line
+    if len(theline) == 0:               # if there are no more lines
+        break                           # break loop
+
+    print(theline, end=" ")             # process what's been read
+
+mynewhandle.close()
