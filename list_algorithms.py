04c8f9e808e0dc995e42683a5ca81ad88f76be8f
Anne Fraysse <fraysse.anne@gmail.com>
2016-10-03 21:50:26 -0700

    worked on merge sort algorithm

--- list_algorithms.py
+++ list_algorithms.py
@@ -20,6 +20,12 @@ def search_linear(xs, target):
 
 ########## LINEAR SEARCH ALGORITHM - MORE REALISTIC ################
 
+# Runtime Complexity: O(n) - may have to scan the whole list 
+# Space Complexity: O(1)
+
+# if the list is ordered, but applying linear search - you can increment the 
+# index at a faster rate (ex: 2); reduces the number of comparisons in list
+
 # Variables
 vocab = "apple boy dog down fell girl grass the tree".split(" ")
 book_words = "the apple fell from the tree to the grass".split(" ")
@@ -122,6 +128,81 @@ k = ceil(log(N + 1, 2))
 # >>> ceil(log(1000 + 1, 2))
 # 10.0
 
+# BINARY SEARCH VS. DIVIDE AND CONQUER 
+    # D&C should have two disjoint recursive calls (ie. QuickSort)
+    # Binary search does not have this, even if it can be implemented recursively
+
+""" REMOVING ADJACENT DUPLICATES """
+
+def remove_adjacent_dups(xs):
+    """ Using list comprehension"""
+    result = []
+    [result.append(i) for i in xs if i not in result]
+    return result
+
+# OR 
+
+def remove_adjacent_dups(xs):
+    result = []
+    most_recent_elem = None
+    for e in xs:
+        if e != most_recent_elem:
+            result.append(e)
+            most_recent_elem = e
+
+    return result
+
+all_words = get_words_in_book("AliceInWonderland.txt")
+all_words.sort()
+book_words = remove_adjacent_dups(all_words)
+print("There are {0} words in the book. Only {1} are unique.".format(len(all_words), len(book_words)))
+print("The first 100 are\n{0}.".format(book_words[:100]))
+
+""" MERGE TWO SORTED LISTS. """
+
+# Deivse an algorithm to merge two sorted lists together. 
+
+def merge_sort(xs, ys):
+    """ An inefficent method."""
+
+    new_list = xs + ys 
+    return new_list.sort()
+
+# OR 
+
+def merge_sort(xs, ys):
+
+    xy = xy.insert(-1, ys)
+    xy.sort()
+
+# OR 
+
+def merge_sort(xs, ys):
+
+    result = []
+    xi = 0
+    yi = 0 
+
+    while True:
+        if xi >= len(xs):       # if xs list is finished
+            result.extend(ys[yi:])
+            return result
+
+        if yi >= len(ys):
+            result.extend(xs[xi:])
+            return result 
+
+        # both lists still have items, copy smaller item to result 
+        if xs[xi] <= ys[yi]:
+            result.append(xs[xi])
+            xi += 1 
+
+        else:
+            result.append(ys[yi])
+            yi += 1
+
+    return result 
+
 ############################## TEST FUNCTION ###################################
 
 # test function 
@@ -152,17 +233,24 @@ def test_suite():
     for (i,v) in enumerate(xs):
         test(search_binary(xs, v) == i)
 
+    # REMOVING ADJACENTS ALGORITHM
+    test(remove_adjacent_dups([1,2,3,3,3,3,5,6,9,9]) == [1,2,3,5,6,9])
+    test(remove_adjacent_dups([]) == [])
+    test(remove_adjacent_dups(["a", "big", "big", "bite", "dog"]) ==
+                                   ["a", "big", "bite", "dog"])
+
+    # MERGE SORTED LISTS
+    xs = [1,3,5,7,9,11,13,15,17,19]
+    ys = [4,8,12,16,20,24]
+    zs = xs+ys
+    zs.sort()
+    test(merge(xs, []) == xs)
+    test(merge([], ys) == ys)
+    test(merge([], []) == [])
+    test(merge(xs, ys) == zs)
+    test(merge([1,2,3], [3,4,5]) == [1,2,3,3,4,5])
+    test(merge(["a", "big", "cat"], ["big", "bite", "dog"]) ==
+                   ["a", "big", "big", "bite", "cat", "dog"])
 # run tests
 test_suite()
-
-
-
-
-
-
-
-
-
-
-
 
